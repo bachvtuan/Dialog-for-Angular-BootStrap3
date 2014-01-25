af6ab828852f99da00c03244ed3d9973502419e0
Tuan Bach Van <bachvtuan@gmail.com>
2014-01-25 19:56:44 +0700

    add focus directive

--- examples/js/app.js
+++ examples/js/app.js
@@ -12,3 +12,27 @@ app.run(function($rootScope,$dialogs) {
   }
 });
 
+
+//Focus on textbox, button
+app.directive('focusMe', function($timeout, $parse) {
+  return {
+    //scope: true,   // optionally create a child scope
+    link: function(scope, element, attrs) {
+      var model = $parse(attrs.focusMe);
+      scope.$watch(model, function(value) {
+        console.log('value=',value);
+        if(value === true) { 
+          $timeout(function() {
+            element[0].focus(); 
+          });
+        }
+      });
+      // to address @blesh's comment, set attribute value to 'false'
+      // on blur event:
+      element.bind('blur', function() {
+         console.log('blur textbox');
+         //scope.$apply(model.assign(scope, false));
+      });
+    }
+  };
+});
